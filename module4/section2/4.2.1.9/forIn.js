bb295bc1ea16fd5745e1fb9904808ddd5b3f764f
Victor Tuy <31480086+vhtc@users.noreply.github.com>
2024-07-10 14:03:12 -0300

    for in

---
+++ module4/section2/4.2.1.9/forIn.js
@@ -0,0 +1,11 @@
+let biblioteca = {
+    name: "Percy Jackson",
+    Autor: "Rick Riordan",
+    pages: 66
+}
+
+for (let livro in biblioteca) {
+    console.log(livro)
+    console.log(biblioteca[livro])
+}
+// mostrar quais são as keys
