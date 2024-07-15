116a4c7bd8061501a8dda6e824a15e18d9531aa1
vhtc <victor.tuy@hotmail.com>
2024-07-15 14:40:30 -0300

    setTimeout

--- module5/section2/5.2.1.8/setTimeout.js
+++ module5/section2/5.2.1.8/setTimeout.js
@@ -1,8 +1,16 @@
-function contar(x) {
+var x = 0
+
+function cont() {
     console.log(`${x} segundos`)
-}
+    
+    if (x < 5) {
+        // setTimeout(() => {
 
-for (let cont = 0; cont < 5; cont++) {
-
-    setTimeout(contar(cont), 1000)
+        //     cont()
+        // }, 1000)
+        
+        setTimeout(cont, 1000)
+        x++
+    }
 }
+cont()
