f4ff2e26065add0cca3d2774c200b764b7b80558
vhtc <victor.tuy@hotmail.com>
2024-07-17 14:08:02 -0300

    setInterval.js

---
+++ module5/section2/5.2.1.9/setInterval.js
@@ -0,0 +1,13 @@
+// setInterval(mostrador, 1000)
+let x = 1
+function mostrador() {
+    console.log(x)
+    x++
+}
+
+contador = setInterval(mostrador, 1000)
+
+
+setTimeout(() => {
+    clearInterval(contador) //quando der 5 segundos pare o contador que mostra o intervalo de x
+}, 5000);
