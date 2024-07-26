a0d7ebc3c099940d663017dc62c457cb5119b3c7
vhtc <victor.tuy@hotmail.com>
2024-07-26 09:45:52 -0300

    module6 errors

---
+++ module6/section1/6.1.1.4/test1.js
@@ -0,0 +1,3 @@
+let mult = (a, b) => a * b
+let result = mult(5, 2)
+console.log(result)
---
+++ module6/section1/6.1.1.4/test2.js
@@ -0,0 +1,6 @@
+try {
+    console.log('abc'); // -> abc
+    conole.log('abc');
+} catch (error) {  
+    console.log(error.message); // -> conole is not defined 
+}
